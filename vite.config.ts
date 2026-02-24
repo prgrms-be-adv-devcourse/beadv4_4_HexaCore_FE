8a71fd66ffcbeaa31324027d8bff66769aec74e7
Seung-9 <ssg9505fj22@naver.com>
2026-02-24 14:24:40 +0900

    fix: vite proxy 수정 - #84

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  const gatewayTarget = env.VITE_BACKEND_GATEWAY || 'http://localhost:9000';
+  const gatewayTarget = env.VITE_BACKEND_GATEWAY || 'https://api.resello.co.kr';
 
   return {
     plugins: [react()],
